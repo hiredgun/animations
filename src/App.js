97a913b69af6c3cf72a4ad12ec80db776ca39408
Konrad Turczyński <konrad.turczynski@schibsted.com>
2019-05-10 15:47:27 +0200

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
    return (
-      <div className="App">
-         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-               Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-               Learn React
-            </a>
-         </header>
-      </div>
+      <svg
+         xmlns="http://www.w3.org/2000/svg"
+         className="App-logo"
+         width="45px"
+         height="51px"
+         viewBox="0 0 200 51"
+         preserveAspectRatio="xMinYMin"
+      >
+         <defs>
+            <path id="path-1" d="M0 .877v25.416h22.036V.877z" />
+            <path id="path-3" d="M12.004 39H23.02V.877H.988V39h11.016z" />
+            <path id="path-5" d="M22.02 19.231V.169H.011v19.062H22.02z" />
+            <path id="path-7" d="M11.005 20H0V.938h22.01V20H11.006z" />
+         </defs>
+         <g id="design" fill="none" fillRule="evenodd">
+            <g id="stream-video-list-desktop-no-open-videos" transform="translate(-22 -13)">
+               <g id="topp" transform="translate(22 12)">
+                  <g id="stream-logo">
+                     <g id="Group-3" transform="translate(0 12.831)">
+                        <mask id="mask-2" fill="#fff">
+                           <use xlinkHref="#path-1" />
+                        </mask>
+                        <path
+                           d="M22.01 26.293l.026-12.68C11.015 7.25 10.994 7.286 0 .877v12.708l22.01 12.708z"
+                           id="Fill-1"
+                           fill="#0088A3"
+                           mask="url(#mask-2)"
+                        />
+                     </g>
+                     <g id="Group-6" transform="translate(21 12.831)">
+                        <mask id="mask-4" fill="#fff">
+                           <use xlinkHref="#path-3" />
+                        </mask>
+                        <path
+                           id="Fill-4"
+                           fill="#4CBDD4"
+                           mask="url(#mask-4)"
+                           d="M1.01 26.293l11.006-6.355L.988 13.585 1.01.877l22.01 12.708v12.708L1.01 39z"
+                        />
+                     </g>
+                     <g id="Group-9" transform="translate(22 .831)">
+                        <mask id="mask-6" fill="#fff">
+                           <use xlinkHref="#path-5" />
+                        </mask>
+                        <path
+                           id="Fill-7"
+                           fill="#005C7A"
+                           mask="url(#mask-6)"
+                           d="M.01.169v12.734l11.006 6.328 11.005-6.354z"
+                        />
+                     </g>
+                     <path id="Fill-10" fill="#A7D8E1" d="M0 13.708l11.005 6.354 11.006-6.328V1z" />
+                     <g id="Group-14" transform="translate(0 31.831)">
+                        <mask id="mask-8" fill="#fff">
+                           <use xlinkHref="#path-7" />
+                        </mask>
+                        <path
+                           id="Fill-12"
+                           fill="#005C7A"
+                           mask="url(#mask-8)"
+                           d="M22.01 7.293L11.006.938 0 7.293 22.01 20z"
+                        />
+                     </g>
+                  </g>
+               </g>
+            </g>
+         </g>
+      </svg>
    );
 }
 
